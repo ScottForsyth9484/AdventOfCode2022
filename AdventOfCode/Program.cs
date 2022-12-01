bab1e077bd3cb6cd0637c25a3c017fa65c7983dc
Scott Forsyth <Scott.Forsyth@sainsburys.co.uk>
2022-12-01 11:34:29 +0000

    Add project files.

---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DayOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<long> data = GroupedData(File.ReadAllText("input.txt"));
+
+            Console.WriteLine($"new max is {data.Max()}");
+            Console.WriteLine($"new top 3 total is {data.OrderByDescending(m=>m).Take(3).Sum()}");
+
+        }
+
+        static IEnumerable<long> GroupedData (string data) => data.Split("\r\n\r\n").Select(m =>
+                m.Split("\r\n").Select(k => long.Parse(k)).Sum()
+            );
+    }
+}
