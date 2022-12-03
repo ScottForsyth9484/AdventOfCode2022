12794036acd4ff581bc7bdc9ba55d6357e43cf97
Scott Forsyth <Scott.Forsyth@sainsburys.co.uk>
2022-12-03 10:44:43 +0000

    tidy up

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -12,10 +12,10 @@
         static IEnumerable<int> Priority(string[] data) => data.Select(m => 
                 new List<IEnumerable<char>>
                     {
-                        m.Take((int)(m.Length / 2)),
-                        m.Skip((int)(m.Length / 2))
+                        m.Take(m.Length / 2),
+                        m.Skip(m.Length / 2)
                     })
-            .Select(k => k[0].Where(c => k[1].Contains(c)).First())
+            .Select(k => k[0].First(c => k[1].Contains(c)))
             .Select(d=> GetValue(d));
 
 
@@ -24,9 +24,7 @@
                 .Select(m => GroupPriority(m))
                 .Sum();             
 
-        static int GroupPriority(string[] data) => GetValue(data[0].First(m => data[1].Contains(m) && data[2].Contains(m)));
-
-        
+        static int GroupPriority(string[] data) => GetValue(data[0].First(m => data[1].Contains(m) && data[2].Contains(m)));        
 
         static int GetValue(char c) => Char.IsLower(c) ? ((int)c) - 96 : ((int)c) - 38;
         
